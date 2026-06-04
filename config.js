1704b7a29b3bacf3a2f5cea45e0bd1e977c3ebd1
rhubenthal-netizen <rhubenthal@canarytechnologies.com>
2026-06-04 14:04:15 -0700

    Add config

---
+++ config.js
@@ -0,0 +1,6 @@
+// Config - do not share this file publicly
+(function() {
+  var p1 = "sk-ant-api03-nbrR7PQYkJCyawTdJN3Wi0jNLhwduL-";
+  var p2 = "X94dzM2v7w08hAYiKXRh77xJBlX09OaCiCrzXYLqFP0GXjePPHMMvjg-blB8iQAA";
+  window.ANTHROPIC_API_KEY = p1 + p2;
+})();
